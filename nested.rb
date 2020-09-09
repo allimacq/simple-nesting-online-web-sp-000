2b7426a9ce25d1cac067aff1653cfe112dcfac36
Allison MacQueen <allimacq@gmail.com>
2020-09-09 20:35:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
     
-  }.merge(programmer_hash)
+  }}.merge(programmer_hash)
 
-    
+          
 end
 
 def changing_alan
