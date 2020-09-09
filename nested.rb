cd1acc0b4638d5383d63b1433a5d6031d7f41fd2
Allison MacQueen <allimacq@gmail.com>
2020-09-09 20:24:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
      
-     language = programmer_hash[:dennis_ritchie][:languages]
+     language = programmer_hash[:dennis_ritchie][:languages][0]
      return language
 end
 
